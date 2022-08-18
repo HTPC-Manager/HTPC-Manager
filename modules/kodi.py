02f4b45febfffc37a916ab0344a6bf52a79b3865
lordairivis <lord.airivis@gmail.com>
2022-08-18 17:09:24 -0600

    some changes to fix issues with the python2->python3 conversion

--- modules/kodi.py
+++ modules/kodi.py
@@ -8,6 +8,7 @@ import base64
 import socket
 import struct
 from urllib.parse import quote
+from urllib.parse import urlencode
 from jsonrpclib import Server
 from sqlobject import SQLObject, SQLObjectNotFound
 from sqlobject.col import StringCol, IntCol
@@ -885,4 +886,9 @@ class Kodi(object):
     def auth(self):
         """ Generate a base64 HTTP auth string based on settings """
         if self.current.username and self.current.password:
-            return base64.encodestring('%s:%s' % (self.current.username, self.current.password)).strip('\n')
+            dummy_param = 'bla'
+            key_url_encoded = urlencode({dummy_param: self.current.username})[len(dummy_param) + 1:]
+            secret_url_encoded = urlencode({dummy_param: self.current.password})[len(dummy_param) + 1:]
+            credentials = '{}:{}'.format(key_url_encoded, secret_url_encoded)
+            bytes_base64_encoded_credentials = base64.encodebytes(credentials.encode('utf-8'))
+            return bytes_base64_encoded_credentials.decode('utf-8').replace('\n', '')
